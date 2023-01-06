c9f60f82e67093c37f6b0c4c240eb8824734e306
Clara COTTIN <ccottin@e3r6p7.clusters.42paris.fr>
2023-01-06 23:31:34 +0100

    equal

---
+++ equal.hpp
@@ -0,0 +1,19 @@
+#ifndef EQUAL_HPP
+# define EQUAL_HPP
+
+namespace ft {
+
+	template< class InputIt1, class InputIt2 >
+	bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2)
+	{
+		
+	}
+
+	template< class InputIt1, class InputIt2, class BinaryPredicate >
+	bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p)
+	{
+
+	}
+}
+
+#endif
